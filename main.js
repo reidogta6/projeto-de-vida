56988438241dcf690beb15e46b59d49092380461
reidogta6 <138135666+reidogta6@users.noreply.github.com>
2025-05-30 09:12:08 -0300

    ok

---
+++ main.js
@@ -0,0 +1,15 @@
+const botoes = document.querySelectorAll(".botoes");
+const botoes = document.querySelectorAll("aba.conteudo");
+
+for (let i = 0; 1< botoes.leght; j++){
+botoes =[i].onclick = function () {
+
+    for (let j = < botoes.length; j++) {
+        botoes[j].classList.remove("ativo");
+        textos[j].classList.remove("ativo"); 
+    }
+
+botoes[j].classList.add("ativo");
+textos[j].classList.remove("ativo");
+}
+}
